47e0a4c252819f3003517daf2163365a72715b6f
vovan9999 <vova.yanchyk1999@gmail.com>
2025-03-11 01:56:55 +0200

    откат

--- bot.py
+++ bot.py
@@ -1,13 +1,34 @@
 import requests
+import telebot
 
-def is_router_online():
-    try:
-        response = requests.get("http://37.53.93.0", timeout=5)
-        return response.status_code == 200
-    except requests.ConnectionError:
-        return False
+TOKEN = "7225625963:AAHN6T7KLshjsX5R-v_6ZY_Q7Zvpx2NmoDQ"
+ROUTER_IP = "37.53.93.0"  # Наприклад, 93.184.216.34
 
-if is_router_online():
-    print("Роутер в мережі!")
-else:
-    print("Роутер не в мережі!")
+# Створюємо екземпляр бота
+bot = telebot.TeleBot(TOKEN)
+
+# Функція для створення інлайн клавіатури з кнопкою
+def create_inline_keyboard():
+    markup = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton("Перевірити роутер", callback_data="check_router")
+    markup.add(button)
+    return markup
+
+# Команда /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    # Відправляємо повідомлення з кнопкою
+    bot.send_message(message.chat.id, "Привіт! Натисни кнопку, щоб перевірити, чи роутер в мережі.", reply_markup=create_inline_keyboard())
+
+# Обробка натискання на кнопку
+@bot.callback_query_handler(func=lambda call: call.data == "check_router")
+def check_router(call):
+    # Логіка перевірки статусу роутера
+    router_status = "Роутер в мережі ✅"  # Можна змінити логіку на реальну перевірку
+    
+    # Відповідаємо на натискання кнопки
+    bot.answer_callback_query(call.id, text=router_status)
+    bot.send_message(call.message.chat.id, router_status, reply_markup=create_inline_keyboard())
+
+# Запускаємо бота
+bot.polling(none_stop=True)
